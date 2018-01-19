378d22483d1c9992e6791c6ae71955999b7ce63b
pallaviwaghaye <palluwaghaye1104@gmail.com>
2018-01-20 02:44:46 +0530

    SignUpActivity ui

---
+++ Dam/app/src/main/java/com/cdac/projectdemo/ui/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.cdac.projectdemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cdac.projectdemo.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
